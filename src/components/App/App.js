1096dd8c74649a90cdc23f2102db069afb9676f3
denis-fominykh <fominykhdenis@gmail.com>
2020-02-10 15:10:21 +0200

    starting project

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+export class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>GitHub Search Engine</h1>
+      </React.Fragment>
+    );
+  }
+}
