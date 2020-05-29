2ae6620b81b82e2f4307103e896c749087b98e03
denis-fominykh <fominykhdenis@gmail.com>
2020-05-29 20:51:24 +0300

    code refactoring

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,36 +2,38 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Card = ({ user }) => {
-  const {
-    name,
-    location,
-    avatar_url,
-    public_repos,
-    public_gists,
-    followers,
-    following,
-    html_url,
-  } = user;
-
   if (user) {
+    const {
+      name,
+      location,
+      avatar_url,
+      public_repos,
+      public_gists,
+      followers,
+      following,
+      html_url,
+    } = user;
+
     return (
       <CardBlock>
-        <img src={avatar_url} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h4 className="card-title">{name}</h4>
-          <h6 className="card-title">{location}</h6>
-          <p className="card-text">Public repositories: {public_repos}</p>
-          <p className="card-text">Public gists: {public_gists}</p>
-          <p className="card-text">Followers: {followers}</p>
-          <p className="card-text">Following: {following}</p>
-          <a
-            href={html_url}
-            className="btn btn-primary"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            View Profile
-          </a>
+        <div className="card">
+          <img src={avatar_url} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h4 className="card-title">{name}</h4>
+            <h6 className="card-title">{location}</h6>
+            <p className="card-text">Public repositories: {public_repos}</p>
+            <p className="card-text">Public gists: {public_gists}</p>
+            <p className="card-text">Followers: {followers}</p>
+            <p className="card-text">Following: {following}</p>
+            <a
+              href={html_url}
+              className="btn btn-primary"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View Profile
+            </a>
+          </div>
         </div>
       </CardBlock>
     );
