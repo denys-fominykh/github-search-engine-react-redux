4e8556bd38d4e140b441aa248adac7ad2ea8060d
denis-fominykh <fominykhdenis@gmail.com>
2020-05-29 20:37:31 +0300

    adding the react hooks

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,38 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-class Search extends Component {
-  onSearchChange = (event) => {
-    this.props.getUsername(event.target.value);
-  };
+const Search = ({ getUser }) => {
+  const [username, setUsername] = useState('');
 
-  render() {
-    return (
-      <SearchBlock>
+  useEffect(() => {
+    if (username !== '') {
+      getUser(username);
+    }
+  }, [username, getUser]);
+
+  return (
+    <SearchBlock>
+      <div className="search__input">
+        <h1>Start typing GitHub username:</h1>
         <div className="search__input">
-          <h1>Start typing GitHub username:</h1>
-          <div className="search__input">
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
-                <span className="input-group-text" id="basic-addon3">
-                  https://github.com/
-                </span>
-              </div>
-              <input
-                type="text"
-                className="form-control"
-                id="basic-url"
-                aria-describedby="basic-addon3"
-                placeholder="facebook"
-                onChange={this.onSearchChange}
-              />
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon3">
+                https://github.com/
+              </span>
             </div>
+            <input
+              type="text"
+              className="form-control"
+              id="basic-url"
+              aria-describedby="basic-addon3"
+              placeholder="jordwalke"
+              onChange={(event) => setUsername(event.target.value)}
+            />
           </div>
         </div>
-      </SearchBlock>
-    );
-  }
-}
+      </div>
+    </SearchBlock>
+  );
+};
 
 const SearchBlock = styled.div`
   margin: 0 auto;
