78b414e17f16e9ffc460c3764359ac734532e61b
denis-fominykh <fominykhdenis@gmail.com>
2020-05-29 20:13:48 +0300

    creating the get resource item service

---
+++ src/services/githubServices.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { getUserAccount } from '../actions';
+
+const _apiBase = 'https://api.github.com/users';
+const _clientId = '1b9a0ab83be88a82a5b3';
+const _clientSecret = 'd12903ccc4bb851b0d67ca0ead54696eeeddfb86';
+
+const getResourceItem = (username) => (dispatch) => {
+  axios
+    .get(
+      `${_apiBase}/${username}?client_id=${_clientId}&client_secret=${_clientSecret}`,
+    )
+    .then((response) => {
+      if (response.status === 200) {
+        dispatch(getUserAccount(response.data));
+      }
+    });
+};
+
+export default getUserAccount;
